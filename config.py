9954c81b4765654957aa97bc967787ca1888630b
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-02-25 12:32:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://deweyo5270minhluncom:deweyo5270minhluncom@cluster0.9ualtow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shrinkforearn.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "68cfedb75f1da753a331e11428b8adc95ba8c0a7")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "15597af089977d7b56868867823be0b17c76d0f1")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","")
