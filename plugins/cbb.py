f9ed11f858d3e5b53a570f0d06a953d649ba4f42
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-02-26 13:05:37 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,5 @@
-#(©)Codexbotz
+#Telegram- @StupidBoi69
+
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,12 +11,24 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"""<b>```
+            ┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓
+            ┃ Developer : <a href='tg://user?id={OWNER_ID}'>Stupidboi69</a>
+            ┃ Creator: <a href='tg://user?id={OWNER_ID}'> This Person</a>
+            ┃ Language : Python3
+            ┃ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>
+            ┃ Source Code: <a href=https://t.me/Stupidboi69>Talk to him.</a>
+            ┃ Main Channel : <a href=https://t.me/Anime_DownLord>​Click Here​</a>
+            ┃ Anime DownLoad Group : <a href=https://t.me/Anime_DownLoad_Group>Click Here</a>
+            ┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛```</b>""",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton('🌺 Contact him for personal bot 🌸', url=f'https://t.me/Stupidboi69')
+                    ],
+                    [
+                        InlineKeyboardButton("🛟 Close", callback_data = "close")
                     ]
                 ]
             )
@@ -25,4 +38,4 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         try:
             await query.message.reply_to_message.delete()
         except:
-            pas
+            pass
